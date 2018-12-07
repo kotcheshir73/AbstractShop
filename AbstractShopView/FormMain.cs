a09d410389ce24dabee90f1dc2b008002dfb92ee
kotcheshir73 <kotcheshir73@gmail.com>
2018-12-07 22:12:37 +0400

    Forms

---
+++ AbstractShopView/FormMain.cs
@@ -0,0 +1,127 @@
+﻿using AbstractShopServiceDAL.BindingModels;
+using AbstractShopServiceDAL.Interfaces;
+using AbstractShopServiceDAL.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Unity;
+using Unity.Attributes;
+
+namespace AbstractShopView
+{
+    public partial class FormMain : Form
+    {
+        [Dependency]
+        public new IUnityContainer Container { get; set; }
+
+        private readonly IMainService service;
+
+        public FormMain(IMainService service)
+        {
+            InitializeComponent();
+            this.service = service;
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                List<OrderViewModel> list = service.GetList();
+                if (list != null)
+                {
+                    dataGridView.DataSource = list;
+                    dataGridView.Columns[0].Visible = false;
+                    dataGridView.Columns[1].Visible = false;
+                    dataGridView.Columns[3].Visible = false;
+                    dataGridView.Columns[5].Visible = false;
+                    dataGridView.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void клиентыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = Container.Resolve<FormClients>();
+            form.ShowDialog();
+        }
+
+        private void компонентыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = Container.Resolve<FormComponents>();
+            form.ShowDialog();
+        }
+
+        private void изделияToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = Container.Resolve<FormProducts>();
+            form.ShowDialog();
+        }
+
+        private void buttonCreateOrder_Click(object sender, EventArgs e)
+        {
+            var form = Container.Resolve<FormCreateOrder>();
+            form.ShowDialog();
+            LoadData();
+        }
+
+        private void buttonTakeOrderInWork_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.SelectedRows.Count == 1)
+            {
+                int id = Convert.ToInt32(dataGridView.SelectedRows[0].Cells[0].Value);
+                try
+                {
+                    service.TakeOrderInWork(new OrderBindingModel { Id = id });
+                    LoadData();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void buttonOrderReady_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.SelectedRows.Count == 1)
+            {
+                int id = Convert.ToInt32(dataGridView.SelectedRows[0].Cells[0].Value);
+                try
+                {
+                    service.FinishOrder(new OrderBindingModel { Id = id });
+                    LoadData();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void buttonPayOrder_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.SelectedRows.Count == 1)
+            {
+                int id = Convert.ToInt32(dataGridView.SelectedRows[0].Cells[0].Value);
+                try
+                {
+                    service.PayOrder(new OrderBindingModel { Id = id });
+                    LoadData();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void buttonRef_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+    }
+}
---
+++ AbstractShopView/FormProducts.cs
@@ -0,0 +1,94 @@
+﻿using AbstractShopServiceDAL.Interfaces;
+using AbstractShopServiceDAL.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Unity;
+using Unity.Attributes;
+
+namespace AbstractShopView
+{
+    public partial class FormProducts : Form
+    {
+        [Dependency]
+        public new IUnityContainer Container { get; set; }
+
+        private readonly IProductService service;
+
+        public FormProducts(IProductService service)
+        {
+            InitializeComponent();
+            this.service = service;
+        }
+
+        private void FormProducts_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                List<ProductViewModel> list = service.GetList();
+                if (list != null)
+                {
+                    dataGridView.DataSource = list;
+                    dataGridView.Columns[0].Visible = false;
+                    dataGridView.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            var form = Container.Resolve<FormProduct>();
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                LoadData();
+            }
+        }
+
+        private void buttonUpd_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.SelectedRows.Count == 1)
+            {
+                var form = Container.Resolve<FormProduct>();
+                form.Id = Convert.ToInt32(dataGridView.SelectedRows[0].Cells[0].Value);
+                if (form.ShowDialog() == DialogResult.OK)
+                {
+                    LoadData();
+                }
+            }
+        }
+
+        private void buttonDel_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.SelectedRows.Count == 1)
+            {
+                if (MessageBox.Show("Удалить запись", "Вопрос", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    int id = Convert.ToInt32(dataGridView.SelectedRows[0].Cells[0].Value);
+                    try
+                    {
+                        service.DelElement(id);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    LoadData();
+                }
+            }
+        }
+
+        private void buttonRef_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+    }
+}
