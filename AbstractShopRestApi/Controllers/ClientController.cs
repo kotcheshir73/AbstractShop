aa6c3bdfc8440082dcd46dc70127bb1528663d25
kotcheshir73 <kotcheshir73@gmail.com>
2018-12-17 21:46:40 +0400

    Rest API

---
+++ AbstractShopRestApi/Controllers/ClientController.cs
@@ -0,0 +1,57 @@
+﻿using AbstractShopServiceDAL.BindingModels;
+using AbstractShopServiceDAL.Interfaces;
+using System;
+using System.Web.Http;
+
+namespace AbstractShopRestApi.Controllers
+{
+    public class ClientController : ApiController
+    {
+        private readonly IClientService _service;
+
+        public ClientController(IClientService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetList()
+        {
+            var list = _service.GetList();
+            if (list == null)
+            {
+                InternalServerError(new Exception("Нет данных"));
+            }
+            return Ok(list);
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            var element = _service.GetElement(id);
+            if (element == null)
+            {
+                InternalServerError(new Exception("Нет данных"));
+            }
+            return Ok(element);
+        }
+
+        [HttpPost]
+        public void AddElement(ClientBindingModel model)
+        {
+            _service.AddElement(model);
+        }
+
+        [HttpPost]
+        public void UpdElement(ClientBindingModel model)
+        {
+            _service.UpdElement(model);
+        }
+
+        [HttpPost]
+        public void DelElement(ClientBindingModel model)
+        {
+            _service.DelElement(model.Id);
+        }
+    }
+}
---
+++ AbstractShopRestApi/Controllers/ComponentController.cs
@@ -0,0 +1,57 @@
+﻿using AbstractShopServiceDAL.BindingModels;
+using AbstractShopServiceDAL.Interfaces;
+using System;
+using System.Web.Http;
+
+namespace AbstractShopRestApi.Controllers
+{
+    public class ComponentController : ApiController
+    {
+        private readonly IComponentService _service;
+
+        public ComponentController(IComponentService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetList()
+        {
+            var list = _service.GetList();
+            if (list == null)
+            {
+                InternalServerError(new Exception("Нет данных"));
+            }
+            return Ok(list);
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            var element = _service.GetElement(id);
+            if (element == null)
+            {
+                InternalServerError(new Exception("Нет данных"));
+            }
+            return Ok(element);
+        }
+
+        [HttpPost]
+        public void AddElement(ComponentBindingModel model)
+        {
+            _service.AddElement(model);
+        }
+
+        [HttpPost]
+        public void UpdElement(ComponentBindingModel model)
+        {
+            _service.UpdElement(model);
+        }
+
+        [HttpPost]
+        public void DelElement(ComponentBindingModel model)
+        {
+            _service.DelElement(model.Id);
+        }
+    }
+}
